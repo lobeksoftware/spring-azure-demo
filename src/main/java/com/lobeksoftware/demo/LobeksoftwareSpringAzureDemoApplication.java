bc60c3896b702340067a5cf0be2b74f53fae39fb
Andrew Oseghale <aoseghale@lobeksoftware.com>
2023-11-27 20:13:23 -0500

    second commit

--- src/main/java/com/lobeksoftware/demo/LobeksoftwareSpringAzureDemoApplication.java
+++ src/main/java/com/lobeksoftware/demo/LobeksoftwareSpringAzureDemoApplication.java
@@ -11,7 +11,7 @@ public class LobeksoftwareSpringAzureDemoApplication {
 
     @GetMapping("/message")
     public String message() {
-        return "Congrats! App deployed successfully in Azure!";
+        return "Congrats! Demo Application deployed successfully in Azure!";
     }
 
     public static void main(String[] args) {
